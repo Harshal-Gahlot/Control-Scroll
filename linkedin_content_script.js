c654cb032a4f3d28b0a67926ad7b8a6456e35c79
Harshal Gahlot <gahlotharshal@gmail.com>
2025-08-05 14:31:18 +0530

    idk exactly how, but linkedin post bloker is fixed now, i do have a vagur idea but not 100% sure, also it took long time to figure it out!!

--- linkedin_content_script.js
+++ linkedin_content_script.js
@@ -1,32 +1,60 @@
-function core() {
-    if (!location.pathname.startsWith("/feed/")) {
-        console.log("linkedin but not core i.e. not /feed/");
+async function core() {
+
+    console.log("location parthname:",location.pathname);
+    if (!location.pathname.startsWith("/feed/")) { // /tscp-serving/dtag
+        console.log("linkedin but not /feed/");
         setTimeout(core, 1000);
         return;
     }
+
+    const data = await getFromStorage();
+
+    console.log('DAAATAAA', Date.now() < data.IN_TIME_LIM)
+
+    if (Date.now() < data.IN_TIME_LIM) {
+        console.log("TIME OVER")
+        displayMessage('Enough posts read for now!')
+        return;
+    }
+
     console.log("linkedin feed encountered");
 
-    const data = getFromStorage('aaaaaaaaaaaaaaa');
 
     const posts_urn = document.getElementsByClassName("scaffold-finite-scroll__content")[0];
     let timer = 0;
+    let lim_reached = false;
     const timerID = setInterval(() => {
+        // console.log(timer, "timer going on...", location.pathname);
         timer++;
-        if (timer >= data.IN_TIME_LIM) {
 
-            console.log("time over");
-            displayMessage()
-            clearInterval(timerID)
-            return;
-
-        }
-        if (posts_urn.querySelector(`[data-finite-scroll-hotkey-item="${data.IN_POSTS_LIM - 1}"]`)) {
-            clearInterval(timerID);
+        
+        // if (timer >= data.IN_TIME_LIM) {
+            
+        //     console.log("time over");
+        //     displayMessage();
+        //     clearInterval(timerID);
+        //     return;
+        
+        // }
+        // console.log(posts_urn.getElementsByClassName("fie-impression-container").length);
+        
+        if (lim_reached) {
+            // document.getElementsByClassName("scaffold-layout__sticky-content")[0].style.setProperty('background-color', 'red', 'important')
+            console.log("lim reached... waiting inside timer")
+        } 
+        // console.log(posts_urn.querySelectorAll("h2.feed-skip-link__container").length);
+        else if (posts_urn.querySelector(`[data-finite-scroll-hotkey-item="${data.IN_POSTS_LIM - 1}"]`)) {
+            // clearInterval(timerID);
             // can use posts_urn.nextElementSibling.remove() but this one supports older (IE11) as well
             posts_urn.parentNode.removeChild(posts_urn.nextElementSibling);
+            console.log("URN IS FULL")
+            lim_reached = true;
+            chrome.storage.local.set({
+                IN_TIME_LIM: Date.now() + 1000 * 60 * 60
+            })
 
             let i = 0;
-            const overflowed = posts_urn.querySelector(`[data-finite-scroll-hotkey-item="${data.IN_POSTS_LIM + i}"]`);
+            let overflowed = posts_urn.querySelector(`[data-finite-scroll-hotkey-item="${data.IN_POSTS_LIM + i}"]`);
             while (overflowed) {
                 console.log("remoing overflowed:", data.IN_POSTS_LIM + i);
                 overflowed.remove();
@@ -34,13 +62,17 @@ function core() {
                 overflowed = posts_urn.querySelector(`[data-finite-scroll-hotkey-item="${data.IN_POSTS_LIM + i}"]`);
             }
 
-            return;
+
         }
     }, 1000);
 }
 
 async function getFromStorage() {
-    const resolve = await new Promise((resolve) => {
+    console.log("chrome:", chrome);
+    console.log("chrome.storage:", chrome?.storage);
+    console.log("chrome.storage.local:", chrome?.storage?.local);
+
+    const res = await new Promise((resolve) => {
         chrome.storage.local.get(['in_posts_seen',
             'in_time_spend',
             'in_posts_speer',
@@ -51,30 +83,29 @@ async function getFromStorage() {
             resolve);
     });
 
-    const in_posts_seen = resolve.in_posts_seen ?? 0;
-    const in_time_spend = resolve.in_time_spend ?? 0;
-    const in_posts_speer = resolve.in_posts_speer ?? 0;
-    const IN_TIME_LIM = resolve.IN_TIME_LIM ?? 10;
-    const IN_POSTS_LIM = resolve.IN_POSTS_LIM ?? 10;
-    const in_hourly_reset = resolve.in_hourly_reset ?? 0;
-    const in_daily_reset = resolve.in_daily_reset ?? 0;
+    const in_posts_seen = res.in_posts_seen ?? 0;
+    const in_time_spend = res.in_time_spend ?? 0;
+    const in_posts_speer = res.in_posts_speer ?? 0;
+    const IN_TIME_LIM = res.IN_TIME_LIM ?? 10;
+    const IN_POSTS_LIM = res.IN_POSTS_LIM ?? 10;
+    const in_hourly_reset = res.in_hourly_reset ?? 0;
+    const in_daily_reset = res.in_daily_reset ?? 0;
+
+    console.log("in getFromStorage");
 
     return { in_posts_seen, in_time_spend, in_posts_speer, IN_TIME_LIM, IN_POSTS_LIM, in_hourly_reset, in_daily_reset };
 
 }
 
 function displayMessage(msg) {
-    document.documentElement.innerHTML = `
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <title>Shorts Limit Reached</title>
-</head>
-<body style="margin:0; padding:0; height:100vh; width:100vw; display:grid; place-items:center; background-color:black;">
-    <h1 style="background-color:#f00; padding:20px; margin:0;">${msg}</h1>
-</body>
-</html>`;
+    console.log("in displayMessage");
+    const urn = document.getElementsByClassName("scaffold-finite-scroll--finite")[0]
+    console.log(urn)
+    urn.innerHTML = msg;
 }
+
+console.log("chrome:", chrome);
+console.log("chrome.storage:", chrome?.storage);
+console.log("chrome.storage.local:", chrome?.storage?.local);
 
 core();
